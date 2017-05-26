42619e8aca26226f92f43638bfb2c8665bfcfabe
shivanipatel116 <shivanirushipatel@gmail.com>
2017-05-26 16:24:46 +0100

    Add files via upload

--- Case-Study-Student-master/CaseStudySudent/js/mainScript.js
+++ Case-Study-Student-master/CaseStudySudent/js/mainScript.js
@@ -116,4 +116,24 @@ $(".menu-items li").on("click", function(){
         $("span.navBtn").click();
         }
     });
+        
+
+
+/*
+function displayLocation(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
             
+            var pLocation = document.getElementById("location");
+            pLocation.innerHTML += latitude + ", " + longitude + "<br>";
+        }
+        
+        window.onload = function() {
+            if(navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(displayLocation);
+            } else {
+                alert("Sorry, this browser doesn't support Geolocation!");
+            }
+        }
+*/
+
